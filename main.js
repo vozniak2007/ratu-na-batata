fc9c292daac36355250ed95c9ba81dd8cdb6ab2f
Maria Eduarda Vozniak <102527180+vozniak2007@users.noreply.github.com>
2024-04-22 11:55:52 -0300

    aaaaa

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ for (let i = 0; i < botoes.length; i++) {
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-11-05T00:00:00");
 const tempoObjetivo2 = new Date("2024-11-05T00:00:00");
-const tempoObjetivo3 = new Date("2024-11-30T00:00:00");
+const tempoObjetivo3 = new Date("2024-11-08T00:00:00");
 const tempoObjetivo4 = new Date("2024-11-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
